2e7ab05a18d8f7c49a63ba44ed454d1bd35d6bd7
Procedurally-Generated-Human <87566207+Procedurally-Generated-Human@users.noreply.github.com>
2021-10-08 21:56:13 +0330

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,14 +5,14 @@
 std::string board[3][3] = {{" ", " "," "},{" "," "," "},{" "," "," "}};;
 bool ai_first = true;
 bool game_is_finished = false;
+char winner = 'n';
 
 
 void draw_board(std::string map[3][3]);
 int get_coordinates(int position);
 void player_1_move();
 void player_2_move();
-
-
+char check_if_game_is_finished(std::string map[3][3]);
 
 
 
